c39e5b5afeb1dd21509e81e20a14ad8c42d0136b
Nathan Whitmore <nwhitmore.work@gmail.com>
2018-06-17 19:36:01 +1000

    Revied method for lambda_PM

--- R/lambda_PM.R
+++ R/lambda_PM.R
@@ -3,5 +3,5 @@ lambda_PM <- function(A, b, W, La, P){
     (P*L^-1) + (La*b*L^(-A))-(La*b*P^(W-A+1))*L^(-1*(W+1)) -1
   }
   growth.estimate <-uniroot.all(func, c(0, 8))[2]
-  growth.estimate
+  return(list("Lambda:"=growth.estimate))
 }
