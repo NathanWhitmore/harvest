d93662002ab87113a756924324a01da376ae50b3
Nathan Whitmore <nwhitmore.work@gmail.com>
2018-06-18 21:10:54 +1000

    Now reproducdible with lambda_PM examples

--- R/lambda_matrix.R
+++ R/lambda_matrix.R
@@ -1,18 +1,20 @@
 # Quick stage based population projection matrix
 
-lambda_matrix <- function(f, b, stages, S){
+lambda_matrix <- function(f, b, stages, S, terminal){
+  if(missing(terminal)) {terminal=FALSE} else {terminal=terminal}
 
-    Fec <- b * S
-    Fec <- c(rep(0,f-1),Fec[f:length(S)])
+    Fec <- c(rep(0,f-1),rep(b,stages-(f-1)))
+    Fec <- S * Fec
     Sur <- diag(S)
     mat <-rbind(Fec,Sur)
     rownames(mat) <- c("Fecundity", rep("Survival", length(S)))
-    mat [stages,stages] <- S[stages]
+    if(terminal==FALSE){
+       mat [stages,stages] <- S[stages]} else {
+         mat [stages,stages] <- 0
+       }
     mat <- mat[-(stages+1),]
     return(list("Transition matrix" =mat,"Lambda:"=lambda(mat)))
 }
-
-
 
 
 
