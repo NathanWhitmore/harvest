7ec8e5e890f9b2883a449da8a4ee6ceb5e6aaae5
Nathan Whitmore <nwhitmore.work@gmail.com>
2018-07-02 09:09:33 +1000

    various eample changes

---
+++ R/buffer_req.R
@@ -0,0 +1,8 @@
+# computes an empirical cumulative distribution function
+# for either net productivity (default) or gross productivity
+
+buffer_req <- function(x, net){
+  if(missing(net)) {net=TRUE} else {net=net}
+  ifelse(net==FALSE,ecdf(Monte.Carlo$gross.productivity)(x),
+         ecdf(Monte.Carlo$net.productivity)(x))
+}
