8e146a26df297269f2672bbcecdd59a8773dcb24
Nathan Whitmore <nwhitmore.work@gmail.com>
2018-07-02 09:09:52 +1000

    delete

--- R/prob_dist.R
+++
@@ -1,8 +0,0 @@
-# computes an empirical cumulative distribution function
-# for either net productivity (default) or gross productivity
-
-prob_dist <- function(x, net){
-  if(missing(net)) {net=TRUE} else {net=net}
-  ifelse(net==FALSE,ecdf(Monte.Carlo$gross.productivity)(x),
-         ecdf(Monte.Carlo$net.productivity)(x))
-}
