c99769a0d4df2bd40651d437e7802661a12021b7
Nathan Whitmore <nwhitmore.work@gmail.com>
2018-07-03 21:03:24 +1000

    Updated

--- R/buffer_req.R
+++ R/buffer_req.R
@@ -1,5 +1,6 @@
 # computes an empirical cumulative distribution function
 # for either net productivity (default) or gross productivity
+# using Monte Carlo data frame
 
 buffer_req <- function(x, net){
   if(missing(net)) {net=TRUE} else {net=net}
